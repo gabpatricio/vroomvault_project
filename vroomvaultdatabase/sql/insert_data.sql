7076fbedc868f3b036eec4a6a43af0d59351b86f
Gabriela Patrício <gabbiepatricio@icloud.com>
2023-12-24 00:45:24 -0300

    Update insert_data.sql

--- vroomvaultdatabase/sql/insert_data.sql
+++ vroomvaultdatabase/sql/insert_data.sql
@@ -1,1 +1,21 @@
+USE vroomvault;
 
+INSERT INTO countries (country) VALUES
+  ('South Korea'), 
+  ('Japan'), 
+  ('Italy'), 
+  ('United Kingdom'), 
+  ('France'), 
+  ('Germany');
+
+INSERT INTO brands (brand, country_id) VALUES
+  ('Hyundai', 1), 
+  ('Toyota', 2), 
+  ('Fiat', 3), 
+  ('Rolls-Royce', 4), 
+  ('Renault', 5), 
+  ('Audi', 6), 
+  ('Mitsubishi', 2), 
+  ('Kia', 1);
+
+INSERT INTO inventory(model, transmission, fuel_type, brand_id)
